4e55dccbc7afe97884ed68e5a0bf0dfb99f0f413
John Afaghpour <johnafaghpour@gmail.com>
2021-06-30 11:55:25 +0200

    added graph of processes

--- graph.py
+++ graph.py
@@ -1,16 +1,31 @@
-from parsing import Stock, Process, Optimize
-import path
+from collections import defaultdict
 
 class Graph:
     """
     Class representing a graph of processes connected to each other by the stock they produce/need.
     """
 
-    def __init__(self, parser):
-        pass
+    def __init__(self, processes, stocks, optimize):
+        self.stocks = stocks
+        self.processes = processes
+        self.optimize = optimize
+        self.needs = defaultdict(list)
+        self.produces = defaultdict(list)
 
     def build_graph(self):
-        pass
+        """
+        Method that builds a graph by regrouping and sort processes by their needs and products
+        """
+        for process in self.processes.values():
+            for stock in process.need:
+                self.needs[stock].append(process)
+            for stock in process.result:
+                self.produces[stock].append(process)
+
+        for stock, processes in self.needs.items():
+            processes.sort(key= lambda p: p.need[stock])
+        for stock, processes in self.produces.items():
+            processes.sort(reverse=True, key= lambda p: p.result[stock])
 
     def get_possible_paths(self):
         pass
--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import utils
-import parsing
-import graph
-import solver
+from parsing import Parser
+from graph import Graph
+from solver import Solver
 
 if __name__ == "__main__":
     # Parsing of CLI arguments
     options = utils.args_parsing()
 
     # Parsing of configuration file
-    parser = parsing.Parser(options)
+    parser = Parser(options)
     parser.main_parsing()
     parser.verify_parsing_content()
 
     # Building a graph of processes
-    graph = graph.Graph(parser)
+    graph = Graph(parser.content, parser.stocks, parser.optimize)
+    graph.build_graph()
+
+    # Debug graph
+    if parser.debug:
+        for stock, processes in graph.needs.items():
+            print(f"\nProcesses that needs {stock}:")
+            for process in processes:
+                print(f"{process}")
+        for stock, processes in graph.produces.items():
+            print(f"\nProcesses that produces {stock}:")
+            for process in processes:
+                print(f"{process}")
+
 
     # Creating a list of possibles paths of processes (solutions) and their performances
-    graph.get_possible_paths()
-    graph.filter_usefull_paths()
-    graph.evaluate_path_productivity()
+    # graph.get_possible_paths()
+    # graph.filter_usefull_paths()
+    # graph.evaluate_path_productivity()
 
     # Run simulation of paths over many generations to find the fittest path
-    solver = solver.Solver(parser, graph)
-    solution = solver.run_simulation()
+    # solver = Solver(parser, graph)
+    # solution = solver.run_simulation()
